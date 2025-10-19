b0940bb791189a76d78e5d551cbec01604853981
Augusto camati <augustocamati21@gmail.com>
2025-10-19 01:39:55 +0100

    feat: dto response para login no spotify

---
+++ src/main/java/camati/spotify_api/clients/openFeing/dto/LoginResponse.java
@@ -0,0 +1,18 @@
+package camati.spotify_api.clients.openFeing.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class LoginResponse {
+  private String accessToken;
+  private String tokenType;
+  private Integer expiresIn;
+}
