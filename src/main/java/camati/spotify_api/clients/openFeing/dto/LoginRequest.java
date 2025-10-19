a188ca4ca3bbbc93a52ddb49e1c25fbf77ce48f4
Augusto camati <augustocamati21@gmail.com>
2025-10-19 01:39:46 +0100

    feat: dto request para login no spotify

---
+++ src/main/java/camati/spotify_api/clients/openFeing/dto/LoginRequest.java
@@ -0,0 +1,19 @@
+package camati.spotify_api.clients.openFeing.dto;
+
+
+import feign.form.FormProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+  @FormProperty("grant_type")
+  private String grantType;
+  @FormProperty("client_id")
+  private String clientId;
+  @FormProperty("client_secret")
+  private String clientSecret;
+}
